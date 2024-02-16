e9451ccc73056897fce78b6cff06b2cf4245df73
Patrice LASSEY <splassey@gmail.com>
2024-02-16 15:57:30 +0100

    Ajout du Product service

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http' ;
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http : HttpClient) { }
+  public getProducts() :Observable<any[]> {
+
+  return this.http.get<any[]>("http://localhost:8089/products");
+
+  }
+
+  public checkProducts(product:any):Observable<any> {
+
+  return this.http.patch<any>(`http://localhost:8089/products/${product.id}`,{ checked: !product.checked });
+  }
+}
